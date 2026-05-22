6dae1fb34cb816596dda47a6a0e9733f4c0030c4
Priya Maurya <priyamaurya98897@gmail.com>
2026-05-22 23:37:26 +0530

    pattern cpp

---
+++ pattern.cpp
@@ -0,0 +1,130 @@
+//pattern
+
+
+//WAP to print a rectangular pattern
+
+// #include<iostream>
+// using namespace std;
+// int main(){
+//     int m;
+//     cout<<"Enter a number rows: ";
+//     cin >> m;
+    
+//     int n;
+//     cout<<"Enter a number columns: ";
+//     cin >> n;
+
+//     for(int i=1; i<=m; i++){
+//         for(int j =1; j<=n; j++){
+//             cout<<"*";
+//         }
+//         cout<<endl;
+
+//     }
+// }
+
+
+
+//WAP to print square pattern
+
+// #include<iostream>
+// using namespace std;
+// int main(){
+
+//     int n;
+//     cout<<"Enter a number  : ";
+//     cin >> n;
+
+    
+//     for(int i=1; i<=n; i++){
+//         for(int j=1; j<=n; j++){
+            
+//             cout<<j;
+//         }
+//         cout<<endl;
+//     }
+// }
+
+
+
+// important question
+
+// #include<iostream>
+// using namespace std;
+// int main(){
+
+//     int n;
+//     cout<<"Enter a number  : ";
+//     cin >> n;
+
+    
+//     for(int i=1; i<=n; i++){
+//         for(int j=1; j<=n; j++){
+//             cout<<j;
+//         }
+//         cout<<endl;
+//     }
+// }
+
+
+
+//pattern of alphabet using typecast and characters
+
+// #include<iostream>
+// using namespace std;
+// int main(){
+//     int n;
+//     cout<<"Enter a number  : ";
+//     cin >> n;
+//     for(int i=1; i<=n; i++){
+//         for(int j=1; j<=n; j++){
+//             cout<<(char)(j+64);
+//         }
+//         cout<<endl;
+//     }
+// }
+
+
+
+
+//homework
+
+// #include<iostream>
+// using namespace std;
+// int main(){
+
+    // int n;
+    // cout<<"Enter a number  : ";
+    // cin >> n;
+
+    
+//     for(int i=1; i<=n; i++){
+//         for(int j=1; j<=n; j++){
+//             cout<<(char)(j+64);
+//         }
+//         cout<<endl;
+//     }
+// }
+
+
+
+
+
+//printing a triangle pattern
+
+//remember we take column as j) and row as i
+
+// #include<iostream>
+// using namespace std;
+// int main(){
+//     int n;
+//     cout<<"Enter a number  : ";
+//     cin >> n;
+//     for(int i=1;i<=n;i++){
+//         for(int j=1; j<=i; j++){
+//             cout<<"*";
+//         }
+//         cout<<endl;
+//     }
+// }
+

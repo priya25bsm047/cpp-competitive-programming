24290f470258ecf7f06a1820466fac1b1f362ff3
Priya Maurya <priyamaurya98897@gmail.com>
2026-05-27 17:57:09 +0530

    trangle pattern 3

--- trianglepattern3.cpp
+++ trianglepattern3.cpp
@@ -20,33 +20,3 @@ int main(){
         } 
     }
 }
-
-
-
-// #include<iostream>
-// using namespace std;
-
-// int main() {
-//     int n;
-//     cout << "Enter the number n: ";
-//     cin >> n;
-
-//     for(int i = 1; i <= n; i++) {
-
-//         // Odd rows → numbers
-//         if(i % 2 != 0) {
-//             for(int j = 1; j <= i; j++) {
-//                 cout << j;
-//             }
-//         }
-
-//         // Even rows → alphabets
-//         else {
-//             for(int j = 1; j <= i; j++) {
-//                 cout << (char)(j + 64);
-//             }
-//         }
-
-//         cout << endl;
-//     }
-// }

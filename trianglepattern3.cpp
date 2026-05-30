1db674642586473e047cb8c3cc803e4b9e282dba
Priya Maurya <priyamaurya98897@gmail.com>
2026-05-30 10:30:48 +0530

    traingle pattern 3

--- trianglepattern3.cpp
+++ trianglepattern3.cpp
@@ -1,22 +1,24 @@
 #include<iostream>
 using namespace std;
-int main(){
+
+int main() {
     int n;
-    cout<<"Enter the number n: ";
-    cin>>n;
-    for(int i=1; i<=n; i++){
-        if(i%2!=0){
-            for(int j=1; j<=i; j++){
-                cout<<j;
+    cout << "Enter the number n: ";
+    cin >> n;
+
+    for(int i = 1; i <= n; i++) {
+
+        if(i % 2 != 0) {
+            for(int j = 1; j <= i; j++) {
+                cout << j;
             }
-            cout<<endl;
         }
-    
-         else{
-            for(int j=1; j<=i; j++){
-                cout<<(char)(j+64);;
+        else {
+            for(int j = 1; j <= i; j++) {
+                cout << (char)(j + 64);
             }
-            cout<<endl;
-        } 
+        }
+
+        cout << endl;
     }
 }

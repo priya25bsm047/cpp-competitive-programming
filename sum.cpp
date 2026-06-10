60ebd8bccd5f5bf8c26685c926c42569158626c6
Priya Maurya <priyamaurya98897@gmail.com>
2026-06-10 23:39:00 +0530

    sum function

---
+++ sum.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int sum(int n, int m){
+    return n+m;
+
+
+}
+int main(){
+    int n;
+    cout<<"Enter a number : ";
+    cin>>n;
+    int m;
+    cout<<"Enter a number : ";
+    cin>>m;
+    cout<<sum(n,m);
+}

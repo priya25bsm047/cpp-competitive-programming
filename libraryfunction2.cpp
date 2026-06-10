9cfb909f1458d134243acd9cc3ca75044ffa32c0
Priya Maurya <priyamaurya98897@gmail.com>
2026-06-10 23:51:51 +0530

    library function2

---
+++ libraryfunction2.cpp
@@ -0,0 +1,24 @@
+//cuberoot function
+
+// #include<iostream>
+// #include<cmath>
+// using namespace std;
+
+// int main(){
+//     int a;
+//     cout<<"Enter a number : ";
+//     cin>>a;
+//     cout<<cbrt(a);
+
+// }
+
+
+//min and max funtion
+#include<iostream>
+#include<cmath>
+using namespace std;
+
+int main(){
+    cout<<min(12,14);
+
+}
